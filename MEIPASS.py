eae0a41cad70f1ec112dbf21feb4030d8878a6b2
oivalian <109859213+oivalian@users.noreply.github.com>
2024-01-09 11:04:52 +1100

    Created MEIPASS.py file

---
+++ MEIPASS.py
@@ -0,0 +1,24 @@
+def resource_path(relative_path):
+    try:
+        base_path = sys.MEIPASS
+    except Exception:
+        base_path = os.path.abspath(".")
+    return os.path.join(base_path, relative_path)
+
+
+# window|root
+root = ttk.Window(themename='darkly')
+iconPath = resource_path('key.ico')
+root.iconbitmap(iconPath)
+root.title('Password Generator')
+root.geometry('700x600')
+root.maxsize(700, 650)
+root.minsize(700, 650)
+
+
+'''
+replace lines 25-31 in passwordgenerator.py with the above code
+
+run command:
+pyinstaller -w --onefile --icon=key.ico --add-data=key.ico:. passwordgenerator.py
+'''
